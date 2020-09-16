a5056a97be6cf35110cc5ac7153a6094b55eb11d
Parker Manning <parkerlmanning@hotmail.com>
2020-09-16 04:25:50 -0500

    corrected structure of prompt obj

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
 const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
 
-const inquirer = require("inquirer")
-fs.writeFile("ReadMe.md", process.argv[2], function (err) {
-    if (err) {
-        return console.log(err);
-    }
-})
-inquirer.prompt([
-    {
-        type: "input",
-        message: "Enter your Title:",
-        title: "title"
-    },
-    {
-        type: "input",
-        message: "Enter your Project Name",
-        projectName: "projectName"
-    },
-    {
-        type: "input",
-        message: "Enter your Project Description",
-        projectDescription: "projectDescription"
-    },
-    {
-        type: "input",
-        message: "Enter your Table of Contents:",
-        tableOfContent: "tableOfContents"
-    },
-    {
-        type: "input",
-        message: "Enter your Installation Process",
-        installation: "installation"
-    },
-    {
-        type: "input",
-        message: "Enter the Usage here",
-        usage: "usage"
-    },
-    {
-        type: "input",
-        message: "License:",
-        license: "license"
-    },
-    {
-        type: "input",
-        message: "Contributing",
-        contributing: "contributing"
-    },
-    {
-        type: "input",
-        message: "Enter Test Here:",
-        test: "test"
-    },
-    {
-        type: "input",
-        message: "Enter Questions here:",
-        questions: "questions"
-    }
-]).then((title) => {
-    const text = `
-        <h1 align="center">Welcome to ${projectName} 👋</h1>
-    
-`}).catch(error => {
-        if (error.isTtyError) {
-            //prompt couldn't be rendered in the current environment
-            console.log("not in this environment boss");
-        } else {
-            //something else
-            console.log("Houston we have a bigger problem")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptObj() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your Title:",
+            title: "title"
+        },
+        {
+            type: "input",
+            message: "Enter your Project Name",
+            projectName: "projectName"
+        },
+        {
+            type: "input",
+            message: "Enter your Project Description",
+            projectDescription: "projectDescription"
+        },
+        {
+            type: "input",
+            message: "Enter your Table of Contents:",
+            tableOfContent: "tableOfContents"
+        },
+        {
+            type: "input",
+            message: "Enter your Installation Process",
+            installation: "installation"
+        },
+        {
+            type: "input",
+            message: "Enter the Usage here",
+            usage: "usage"
+        },
+        {
+            type: "input",
+            message: "License:",
+            license: "license"
+        },
+        {
+            type: "input",
+            message: "Contributing",
+            contributing: "contributing"
+        },
+        {
+            type: "input",
+            message: "Enter Test Here:",
+            test: "test"
+        },
+        {
+            type: "input",
+            message: "Enter Questions here:",
+            questions: "questions"
         }
-    });
+
+])}
+           // .then((title) => {
+//             const text = `
+//         <h1 align="center">Welcome to ${projectName} 👋</h1>
+    
+// `}).catch(error => {
+//                 if (error.isTtyError) {
+//                     //prompt couldn't be rendered in the current environment
+//                     console.log("not in this environment boss");
+//                 } else {
+//                     //something else went wrong
+//                     console.log("Houston we have a bigger problem")
+//                 }
+//             })
