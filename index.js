a8cb04e35e8be4193826f8e9a28d26b5db09c752
Parker Manning <parkerlmanning@hotmail.com>
2020-09-16 03:58:28 -0500

    adding other files

---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+
+const inquirer = require("inquirer")
+fs.writeFile("ReadMe.md", process.argv[2], function (err) {
+    if (err) {
+        return console.log(err);
+    }
+})
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter your Title:",
+        title: "title"
+    },
+    {
+        type: "input",
+        message: "Enter your Project Name",
+        projectName: "projectName"
+    },
+    {
+        type: "input",
+        message: "Enter your Project Description",
+        projectDescription: "projectDescription"
+    },
+    {
+        type: "input",
+        message: "Enter your Table of Contents:",
+        tableOfContent: "tableOfContents"
+    },
+    {
+        type: "input",
+        message: "Enter your Installation Process",
+        installation: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter the Usage here",
+        usage: "usage"
+    },
+    {
+        type: "input",
+        message: "License:",
+        license: "license"
+    },
+    {
+        type: "input",
+        message: "Contributing",
+        contributing: "contributing"
+    },
+    {
+        type: "input",
+        message: "Enter Test Here:",
+        test: "test"
+    },
+    {
+        type: "input",
+        message: "Enter Questions here:",
+        questions: "questions"
+    }
+]).then((title) => {
+    const text = `
+        <h1 align="center">Welcome to ${projectName} 👋</h1>
+    
+`})
